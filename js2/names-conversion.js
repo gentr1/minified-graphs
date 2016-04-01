f95810e0d4df94756e96ab6a56dd025d87a3a4b4
gentr1 <mehdi.khoury@gmail.com>
2016-04-01 12:40:34 +0100

    minified graphs

---
+++ js2/names-conversion.js
@@ -0,0 +1,14 @@
+
+var map_long_short_names={
+"Antigua and Barbuda":	"Antigua & Barbuda",
+"Bosnia and Herzegovina":	"Bosnia & Herzegovina",
+"Central African Republic":	"Central African Rep",
+"China (Hong Kong SAR)":	"Hong Kong",
+"Micronesia (Federated States of)":	"Micronesia (FS)",
+"Sao Tome and Principe":	"Sao Tome & Principe",
+"Saint Kitts and Nevis":	"St Kitts & Nevis",
+"Saint Vincent and the Grenadines":	"St Vincent & the Grenadines",
+"Occupied Palestinian Territory":	"State of Palestine",
+"Syrian Arab Republic":	"Syria",
+"United States of America":	"USA"
+}
